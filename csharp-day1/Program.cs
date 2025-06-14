234452ac3c67d00c6aec696a48fcca0e8f823b8c
Narendra Gadgil <gadgil.n5@gmail.com>
2025-06-15 00:58:05 +0530

    Program5n6

--- csharp-day1/Program.cs
+++ csharp-day1/Program.cs
@@ -1,23 +1,38 @@
 ﻿//--------------------------------------------------------------------------------------------//
 //1- C# Program to Check Whether a Given Number is Even or Odd
     Console.WriteLine("Enter the Number"); 
-    int num = Console.Read();
-    Console.WriteLine(num % 2 == 0 ? "Even" : "Odd");
+    int numberIsEven = Console.Read();
+    Console.WriteLine(numberIsEven % 2 == 0 ? "Even" : "Odd");
 //-------------------------------------------------------------------------------------------//
 //2- C# Program to Print Odd Numbers in a Given Range
 //---------Type1 Given Range
-int startRange = 1, endRange = 20;
-for ( int i = startRange; i <= endRange; i++)
-{ if (i % 2 != 0) { Console.WriteLine(i); } }
-
+    int startRange = 1, endRange = 20;
+    for ( int i = startRange; i <= endRange; i++)
+    { if (i % 2 != 0) { Console.WriteLine(i); } }
 //---------Type 2 Given Range
-
+    
+//-------------------------------------------------------------------------------------------//
 // C# Program to Check Whether a Number is Positive or Not
+//-------------------------------------------------------------------------------------------//
 // C# Program to Find the Largest of Two Numbers
+//-------------------------------------------------------------------------------------------//
 // C# Program to Swap Two Numbers
+    int firstNumber = 21,secondNumber = 10;
+    Console.WriteLine("First Number is {0} and Second Number is {1}", firstNumber, secondNumber);
+    firstNumber = firstNumber + secondNumber;
+    secondNumber = firstNumber - secondNumber; 
+    firstNumber = firstNumber - secondNumber; 
+    Console.WriteLine("First Number is {0} and Second Number is {1}", firstNumber, secondNumber);
+//-------------------------------------------------------------------------------------------//
 // C# Program to Check if a Number is Divisible by 2
+    int divisibleByNumber = 80;
+    Console.WriteLine(divisibleByNumber % 2 == 0 ? "Divisible by 2" : "Not Divisible by 2");
+//-------------------------------------------------------------------------------------------//
 // C# Program to Find the Sum of All the Multiples of 3 and 5
+//-------------------------------------------------------------------------------------------//
 // C# Program to Print All the Multiples of 17 which are Less than 100
+//-------------------------------------------------------------------------------------------//
 // C# Program to Find Sum of Digits of a Number
+//-------------------------------------------------------------------------------------------//
 // C# Program to Find Sum of Digits of a Number using Recursion
 
