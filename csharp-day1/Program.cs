ef52c477e0cede97b7945dfcc989a249e4c133bb
Narendra Gadgil <gadgil.n5@gmail.com>
2025-06-15 00:01:46 +0530

    Day1-Program1

---
+++ csharp-day1/Program.cs
@@ -0,0 +1,14 @@
+﻿//1- C# Program to Check Whether a Given Number is Even or Odd
+Console.WriteLine("Enter the Number");
+float num = Console.Read();
+Console.WriteLine(num % 2 == 0 ? "Even" : "Odd");
+
+// C# Program to Print Odd Numbers in a Given Range
+// C# Program to Check Whether a Number is Positive or Not
+// C# Program to Find the Largest of Two Numbers
+// C# Program to Swap Two Numbers
+// C# Program to Check if a Number is Divisible by 2
+// C# Program to Find the Sum of All the Multiples of 3 and 5
+// C# Program to Print All the Multiples of 17 which are Less than 100
+// C# Program to Find Sum of Digits of a Number
+// C# Program to Find Sum of Digits of a Number using Recursion
